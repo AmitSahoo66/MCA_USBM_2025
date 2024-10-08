3d0f9d40293e6a7501884a203063255cabb3cc8a
AmitSahoo66 <sahooamitkumar367@gmail.com>
2024-10-08 12:56:28 +0530

    first commit

---
+++ frontend/javaScript/new.js
@@ -0,0 +1,56 @@
+
+// Number
+//let num1 = 50;
+//let decimal_no = 1.0001; 
+//let pi = 3.14;
+//let age = 24;
+//let roll_no = 21;//
+
+
+//console.log(typeof num1); //
+
+
+//Addition
+//let num2 = 100;
+//let num3 = 125;
+//let addition = num2 + num3;
+//console.log(addition);//
+
+//substration
+//let num2 = 100;
+//let num3 = 125;
+//let substration = num2 - num3;
+//console.log(substration);//
+
+//mul
+//let num2 = 100;
+//let num3 = 125;
+//let multipication = num2 * num3;
+//console.log(multipication);//
+
+
+//div
+//let num2 = 100;
+//let num3 = 125;
+//let division = num2/num3;
+//console.log(division);//
+
+
+// symbol
+//let sym1 = symbol("sym1");
+//let sym2 = symbol("sym1");
+//console.log(sym1 === sym2);// 
+
+
+//NaN(Not a Number)
+//let num5 =0;//
+//let num6 = 0;//
+//console.log(num5 / num6);//
+
+
+//
+let name="amit";
+let age1=24;
+let city="bhubaneswar";
+
+console.log('My name is:${name},age is:${age}),and city is:${city}');
